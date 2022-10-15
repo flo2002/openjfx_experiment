e894d3406434428cb9522d15f3f22ecaa5e6ea38
flo2002 <florian.schiemer003@gmail.com>
2022-10-15 07:48:18 +0200

    addPersistence
    java.persistence import does not work, thus the code will not run

---
+++ src/main/java/com/example/openjfx_experiment/subsystems/PersonBroker.java
@@ -0,0 +1,38 @@
+package com.example.openjfx_experiment.subsystems;
+
+import com.example.openjfx_experiment.model.Person;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+public class PersonBroker extends BrokerBase<Person> {
+    private final EntityManager entityManager;
+
+    public PersonBroker(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Person> getAll() {
+        List<Person> persons = (List<Person>) entityManager.createQuery("FROM Person").getResultList();
+        return persons;
+    }
+
+    public void add(Person person) {
+        entityManager.getTransaction().begin();
+        entityManager.persist(person);
+        entityManager.getTransaction().commit();
+    }
+
+    public void update(Person person) {
+        entityManager.getTransaction().begin();
+        entityManager.merge(person);
+        entityManager.getTransaction().commit();
+    }
+
+    public void delete(Person person) {
+        entityManager.getTransaction().begin();
+        entityManager.remove(person);
+        entityManager.getTransaction().commit();
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.example.openjfx_experiment {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.apache.logging.log4j;
-    requires java.persistence;
 
     opens com.example.openjfx_experiment to javafx.fxml;
     exports com.example.openjfx_experiment;
